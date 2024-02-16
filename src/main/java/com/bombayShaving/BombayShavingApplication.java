848d6b68ba662b355952d0cb7732522fd6752c91
SharpCoderVarunGaur <varungaur408@gmail.com>
2024-02-16 14:59:11 +0530

    FirstCommit

---
+++ src/main/java/com/bombayShaving/BombayShavingApplication.java
@@ -0,0 +1,13 @@
+package com.bombayShaving;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BombayShavingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BombayShavingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bombayShaving/BombayShavingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bombayShaving;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BombayShavingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
